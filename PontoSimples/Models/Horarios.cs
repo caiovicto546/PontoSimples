cbb6d0a2ab07be7fdb63cf4b10f6ed820f535477
caiovicto546 <caiovicto@gmail.com>
2019-03-22 14:48:33 -0300

    CRUD para Hoarios

--- PontoSimples/Models/Horarios.cs
+++ PontoSimples/Models/Horarios.cs
@@ -1,24 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PontoSimples.Models
 {
+    [Serializable]
+    [Table("horarios")]
     public class Horarios
     {
+        [Column("id_horario")]
         public int Id { get; set; }
-        public int Codigo { get; set; }
+        [Column("cod_horario")]
+        [Display(Name = "Código")]
+        public string Codigo { get; set; }
+        [Column("hora_inicio")]
+        [Display(Name = "Hora Inicio")]
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{0:HH:MM}")]
         public DateTime HoraInicio { get; set; }
+        [Column("hora_fim")]
+        [Display(Name = "Hora Fim")]
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{0:HH:MM}")]
         public DateTime HoraFim { get; set; }
+        [Column("inicio_almoco")]
+        [Display(Name = "Inicio Almoço")]
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{0:HH:MM}")]
         public DateTime InicioAlmoco { get; set; }
+        [Column("fim_almoco")]
+        [Display(Name = "Fim Almoço")]
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{0:HH:MM}")]
         public DateTime FimAlmoco { get; set; }
 
         public Horarios()
         {
         }
 
-        public Horarios(int id, int codigo, DateTime horaInicio, DateTime inicioAlmoco, DateTime fimAlmoco, DateTime horaFim)
+        public Horarios(int id, string codigo, DateTime horaInicio, DateTime inicioAlmoco, DateTime fimAlmoco, DateTime horaFim)
         {
             Id = id;
             Codigo = codigo;
@@ -28,7 +52,7 @@ namespace PontoSimples.Models
             HoraFim = horaFim;
         }
 
-        public Horarios(int codigo, DateTime horaInicio, DateTime inicioAlmoco, DateTime fimAlmoco, DateTime horaFim)
+        public Horarios(string codigo, DateTime horaInicio, DateTime inicioAlmoco, DateTime fimAlmoco, DateTime horaFim)
         {
             Codigo = codigo;
             HoraInicio = horaInicio;
